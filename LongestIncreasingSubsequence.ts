a386eb180c75338afee5f7267a8e9049cb717de7
joel garcia <jgarcia27@ggc.edu>
2021-07-11 13:08:16 -0400

    reverseInteger

--- LongestIncreasingSubsequence.ts
+++ LongestIncreasingSubsequence.ts
@@ -1,9 +1,31 @@
+
 function lengthOfLIS(nums: number[]): number {
-    let count = 0;
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] < nums[i + 1]){
-            count++;
-        }      
+    let count, max, i = 0;
+    let temp = [];
+    let lis = Array(nums.length).fill(0);
+    for (let i; i < nums.length; i++) {
+        lis[i] = 1;
     }
-    return count;
+    
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < i; j++){
+            if(nums[i] > nums[j] && lis[i] < lis[j+1]){
+                lis[i] = lis[j+1];
+            }
+        }   
+    }
+    for(i = 0; i < nums.length; i++){
+        if(max < lis[i]){ max = lis[i];}
+    }
+
+
+    
+
+    // for(let i = 0; i < temp.length; i++){
+    //     if(temp[i] >= temp[i+1]){
+    //         temp.pop();
+    //     }
+    // }
+    // console.log("length:" + temp);
+    return max;
 };
